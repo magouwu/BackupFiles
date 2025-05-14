afa6fcb35180c100c4796066910b41b2a95bf55e
magouwu <93410612+magouwu@users.noreply.github.com>
2025-05-14 08:46:52 -0600

    Update BackupFilesFromTo.ps1
    translate spanish to english comments

--- BackupFilesFromTo.ps1
+++ BackupFilesFromTo.ps1
@@ -11,17 +11,19 @@ Get-ChildItem -Recurse $source | Where-Object {
     $relativePath = $_.FullName.Substring($source.Length)
     $destFile = Join-Path $destination $relativePath
      if (Test-Path $destFile) {
-        # Comparar contenido
+        # Comparing content
         $hashSource = Get-FileHash $_.FullName
         $hashDest = Get-FileHash $destFile
+        # if exist, delete file
          if ($hashSource.Hash -eq $hashDest.Hash) {
             Write-Host "Ya existe (idéntico): $destFile"
             Remove-Item $_.FullName -Force
         } else {
+        #if exist but its different content, dont do nothing
             Write-Host "Conflicto (mismo nombre, diferente contenido): $destFile"
         }
     } else {
-        # No existe → moverlo al destino
+        #doesnt exist → move to destination
         $targetDir = Split-Path $destFile
         New-Item -ItemType Directory -Path $targetDir -Force | Out-Null
         Move-Item $_.FullName -Destination $destFile
